82f7d4933cb030f8b731d29c2d7b70adc3e1bdfd
sheabali <iamskpranto@gmail.com>
2025-01-02 22:46:03 +0600

    redux store setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Counter With Redux</h1>
+        <button>Increment</button>
+        <div>0</div>
+        <button>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
