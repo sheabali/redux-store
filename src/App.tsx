15efc2f8a791b96c0b4e48c7f0bcfb23d51664b2
sheabali <iamskpranto@gmail.com>
2025-01-03 22:53:41 +0600

    initial set up completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { decrement, increment } from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { count } = useAppSelector((state) => state.counter);
-
-  const handleIncrement = (count: number) => {
-    dispatch(increment(count));
-  };
-  const handleDecrement = () => {
-    dispatch(decrement());
-  };
-
+  
   return (
     <>
-      <div>
-        <h1>Counter With Redux</h1>
-        <Button onClick={() => handleIncrement(1)}>Increment</Button>
-        <Button onClick={() => handleIncrement(5)}>Increment by 5</Button>
-        <div>{count}</div>
-        <Button onClick={handleDecrement}>Decrement</Button>
-      </div>
+     <Navbar/>
     </>
   );
 }
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import Logo from "@/assets/logo";
+
+export default function Navbar () {
+  return(
+    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+      <div className="flex items-center">
+        <Logo/> <span className="font-bold ml-2">Task</span>
+      </div>
+    </nav>
+  )
+}
